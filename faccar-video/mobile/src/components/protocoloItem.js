d72b22b309e6d38c666486889d4a4084873a82b4
Hericson Ramos <hericson@bwfstudio.com.br>
2020-11-16 21:49:43 -0300

    Update protocoloItem.js

--- faccar-video/mobile/src/components/protocoloItem.js
+++ faccar-video/mobile/src/components/protocoloItem.js
@@ -1,73 +1,75 @@
 import React from 'react';
-import {View, Text, Animated, StyleSheet} from 'react-native';
+import {View, Text,StyleSheet, Animated} from 'react-native';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-export default function protocoloItem({data, HandleLeft, HandleRight}){
+export default function ProtocoloList({data, handleLeft, handleRight}){
 
-    function LeftAction(progress, dragX){
-        const escala = dragX.interpolate({
-            inputRange : [0,100],
-            outputRange : [0,1],
-            extrapolate : 'clamp',
-        });
-        return(
-            <View style = {style.LeftAction}>
-                <Animated.Text style = {style.TextAction, {transform : [{scale : escala}]}}>Concluir</Animated.Text>
-            </View>
-        );
-        
-    }
+  function LeftActions(progress, dragX){
 
-    function RightAction(progress, dragX){
-        const escala = dragX.interpolate({
-            inputRange : [-100,0],
-            outputRange : [1.5,0],
-            extrapolate : 'clamp',
-        });
-        return(
-            <View style = {style.RightAction}>
-                <Animated.Text style = {style.TextAction, {transform : [{scale : escala}]}}>Cancelar</Animated.Text>
-            </View>
-        );
-        
-    }
+    const escala = dragX.interpolate({
+      inputRange :  [0 , 100],
+      outputRange : [0, 1],
+      extrapolate : 'clamp'
+    });
 
     return (
-        <Swipeable 
-            renderLeftAction = {LeftAction}
-            onSwipeableLeftOpen = {HandleLeft}
-            renderRightAction = {RightAction}
-            onSwipeableRightOpen = {HandleRight}
-        >    
-            <View style = {style.container}>
-                <Text style = {style.Text}>{data.protocolo}</Text>
-            </View>
-        </Swipeable>
+      <View style={ styles.leftAction}> 
+        <Animated.Text style={ [styles.textAction, {transform : [{ scale : escala}]}  ] }>Concluir</Animated.Text>
+      </View>
     )
+  }
 
+  function RightActions(progress, dragX){
+
+    const escala = dragX.interpolate({
+      inputRange :  [-100 , 0],
+      outputRange : [1.5, 0],
+      extrapolate : 'clamp'
+    });
+
+    return (
+      <View style = {styles.rightAction}>
+        <Animated.Text style={ [styles.textAction, {transform : [{ scale : escala}]}  ] }>Cancelar</Animated.Text>
+      </View>
+    ) 
+  }
+
+  return(
+    <Swipeable
+    renderLeftActions = {LeftActions}
+    onSwipeableLeftOpen = {handleLeft}
+    renderRightActions = {RightActions}
+    onSwipeableRightOpen = {handleRight}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>{data.protocolo}</Text>
+      </View>
+    </Swipeable>
+ 
+  )   
 }
 
-const style = StyleSheet.create({ 
-    container: {
-        backgroundColor: '#FFF',
-        paddingHorizontal: 10,
-        paddingVertical: 20,
-    },  
-    Text: {
-        fontSize: 16,
+const styles = StyleSheet.create({
+  container : {
+    backgroundColor : '#FFF',
+    paddingHorizontal : 10,
+    paddingVertical: 20
+  },
+  text:{
+    fontSize: 16
+  },
+  leftAction:{
+    backgroundColor : "#3DDB43" ,
+    justifyContent :'center',
+    paddingHorizontal: 5,
+    flex :1,
+   },
+    rightAction:{
+    backgroundColor : "#F00",
+    justifyContent :'center',
+    paddingHorizontal: 5
     },
-    LeftAction: {
-        backgroundColor: '#3388e3',
-        justifyContent: 'center',
-        paddingHorizontal: 5,
-        flex:1,
-    },
-    RightAction: {
-        backgroundColor: '#f00',
-        justifyContent: 'center',
-        paddingHorizontal: 5,
-    },
-    TextAction: {
-        color: '#FFF',
-    }
-})
+  textAction:{
+    color : "#FFF"
+  }
+});
